e764a8ece4ce92a81571557b846caff039394f94
Administrator <admin@example.com>
2020-11-18 15:21:31 +0800

    增加测试redis接口

--- my-manage/src/main/java/com/myManage/controller/Test.java
+++ my-manage/src/main/java/com/myManage/controller/Test.java
@@ -1,15 +1,17 @@
 package com.myManage.controller;
 
-import com.netflix.zuul.context.RequestContext;
-import com.utils.PasswdUtil;
-import com.utils.RedisUtils;
+import com.alibaba.fastjson.JSON;
+import com.utils.FileUtils;
+import com.utils.RedisUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
-import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Author: menfeng
@@ -21,12 +23,49 @@ import java.security.Principal;
 public class Test {
 
     @GetMapping("/test")
-    public String test(HttpServletRequest request){
-        String token = request.getHeader("token");
-        log.info("test--token--:{}",token);
+    public String test() {
+       /* String token = request.getHeader("token");
+        log.info("test--token--:{}", token);
         String s = RedisUtils.getRedisUtil().get(token);
         log.info(s);
         String pwd = PasswdUtil.getPwd("1", "admin");
-        return "admin的密码是："+pwd+"<a href='http://127.0.0.1:9084/logout'/>退出";
+        return "admin的密码是：" + pwd + "<a href='http://127.0.0.1:9084/logout'/>退出";*/
+       return "123456";
     }
+
+    @GetMapping("/hello")
+    public String testRedis(){
+        log.info("why");
+        RedisUtil.setString("123","helloworld");
+        return RedisUtil.getString("123");
+    }
+
+    @PostMapping("/upload")
+    public Map<String, Object> upload(@RequestParam("file") MultipartFile file) {
+        log.info("进入upload");
+        long st = System.currentTimeMillis();
+        Map<String, Object> data = new HashMap<>();
+        try {
+            if (file.isEmpty()) {
+                data.put("code", 404);
+                data.put("message", "文件为空");
+                return data;
+            }
+            String path = FileUtils.saveFile(file);
+            data.put("code", 200);
+            data.put("message", "文件上传成功");
+            data.put("fileName", file.getOriginalFilename());
+            data.put("filePath", path);
+        } catch (Exception e) {
+            log.info("upload file failed", e);
+            data.put("code", 500);
+            data.put("message", "上传失败");
+            return data;
+        }
+        long nt = System.currentTimeMillis();
+        log.info(JSON.toJSONString(data));
+        log.info("上传时间为---{}", (nt - st) / 1000 + "s");
+        return data;
+    }
+
 }
