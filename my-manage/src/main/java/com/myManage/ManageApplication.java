3fa2af0f150a768f80688e116c50d2101ef8bdd0
Administrator <admin@example.com>
2020-11-26 16:22:33 +0800

    分片上传

--- my-manage/src/main/java/com/myManage/ManageApplication.java
+++ my-manage/src/main/java/com/myManage/ManageApplication.java
@@ -1,10 +1,14 @@
 package com.myManage;
 
+import com.github.tobato.fastdfs.FdfsClientConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.context.annotation.Import;
+import org.springframework.jmx.support.RegistrationPolicy;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -17,6 +21,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableFeignClients
 @MapperScan({"com.myProject.dao.mapper.**"})
+@Import(FdfsClientConfig.class)
+//解决jmx注册冲突
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
 public class ManageApplication {
     public static void main(String[] args) {
         SpringApplication.run(ManageApplication.class);
--- my-manage/src/main/java/com/myManage/controller/Test.java
+++ my-manage/src/main/java/com/myManage/controller/Test.java
@@ -1,17 +1,9 @@
 package com.myManage.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.utils.FileUtils;
 import com.utils.RedisUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @Author: menfeng
@@ -40,32 +32,6 @@ public class Test {
         return RedisUtil.getString("123");
     }
 
-    @PostMapping("/upload")
-    public Map<String, Object> upload(@RequestParam("file") MultipartFile file) {
-        log.info("进入upload");
-        long st = System.currentTimeMillis();
-        Map<String, Object> data = new HashMap<>();
-        try {
-            if (file.isEmpty()) {
-                data.put("code", 404);
-                data.put("message", "文件为空");
-                return data;
-            }
-            String path = FileUtils.saveFile(file);
-            data.put("code", 200);
-            data.put("message", "文件上传成功");
-            data.put("fileName", file.getOriginalFilename());
-            data.put("filePath", path);
-        } catch (Exception e) {
-            log.info("upload file failed", e);
-            data.put("code", 500);
-            data.put("message", "上传失败");
-            return data;
-        }
-        long nt = System.currentTimeMillis();
-        log.info(JSON.toJSONString(data));
-        log.info("上传时间为---{}", (nt - st) / 1000 + "s");
-        return data;
-    }
+
 
 }
